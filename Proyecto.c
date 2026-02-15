254e0d5c55c50b72b6aa22af950bec8e63163beb
J1suS <jesusrivas1003@gmail.com>
2026-02-15 17:35:25 -0400

    Update Proyecto.c

--- Proyecto.c
+++ Proyecto.c
@@ -2,26 +2,30 @@
 //CONSTANTES
 #define MAX_PROVEEDORES 3
 #define MAX_PRODUCTOS 5
+
 // VARIABLES GLOBALES
 int contProductos=0;
 int posEncontrada=-1;
 int auxCodigo;
-int auxPrecio;
+float auxPrecio;
 int auxTamano;
 int auxCantidad;
 int auxProveedor;
+int auxActivo;
 
 // ESTRUCTURAS
 struct Producto
 {
     //Plantilla de datos para cada producto a ingresar
-    int id;                 //codigo del producto
-    char name[20];          //nombre
-    float tamano;           //cantidad del producto
-    int stock;              //unidades
+    int id;
+    char name[20];
+    float tamano;
+    int stock;
     float precio;
     //int ubicacion;
-    char fechaIngrego[8];   //Fecha de ingreso del producto
+    int dia;
+    int mes;
+    int anio;
     char nombreProveedor[50];
     int activo;
 };
@@ -30,20 +34,20 @@ struct Proveedor //contigo no sé qué hacer aún, pero lo dejo por si acaso
 {
     int rif;
     char nombre[50];
-    char codigo[3];
+    char codigo[4];
 };
 
 struct Proveedor proveedores[MAX_PROVEEDORES] = { 
-    {123456789, "Distribuidora 1", "DA"},
-    {987654321, "Distribuidora 2", "BP"},
-    {123459876, "Distribuidora 3", "PC"}
+    {123456789, "Distribuidora 1", "ABC"},
+    {987654321, "Distribuidora 2", "XYZ"},
+    {123459876, "Distribuidora 3", "DEF"}
 };
 
 struct Producto productos[MAX_PRODUCTOS]; //donde se guarda la info
 
 //FUNCIONES
 int menu();
-void noHallado();// no entiendo por qué si te escribo aquí sirves, pero estando allí arranca sim problemas
+void noHallado();
 int codigo();
 void nombre();
 int tamano();
@@ -51,76 +55,90 @@ int stock();
 float precio();
 void fechaIngreso();
 void nombreProveedor();
+int activo();
 
 
-int menu()// función menú, muestra las opciones disponibles y valida la entrada del usuario
+int menu()
 {
     int option;//guarda la opción ingresada por el usuario
 
     do
     {
-        printf("\nALMACEN CAFE\n");
+        printf("===========================================================\n");
+        printf("\t\t\tALMACEN CAFE\n");
+        printf("===========================================================\n");
         printf("[ 1 ] Ingresar\n");
         printf("[ 2 ] Mostrar\n");
         printf("[ 3 ] Eliminar\n");
         printf("[ 4 ] Retirar\n");
         printf("[ 5 ] Consultar\n");
         printf("[ 6 ] Salir\n");
+        printf("===========================================================\n");
         scanf("%d", &option);
 
         if (option < 1 || option > 6)//valida que la opción ingresada esté dentro del rango permitido
         {
             printf("\nOpcion invalida, intente de nuevo.\n");
         }
-    }while (option < 1 || option > 6);//si la opción es inválida, se repetirá el menú hasta que se ingrese una opción válida
-    
-    return option;//devuelve la opción seleccionada por el usuario para que pueda ser utilizada en el programa principal
+    }while (option < 1 || option > 6);
+
+    return option;
 }
 
 int codigo()
 {
+    printf("===========================================================\n");
     printf("ID: ");
-        scanf("%d", &auxCodigo); //<- guarda el valor ingresado en la variable auxiliar para asignarlo  al campo id de la estructura Producto
+    scanf("%d", &auxCodigo); //<- variable auxiliar para validar
+    
     //validación del código
-        while(auxCodigo<0)
-         {
-            printf("Codigo incorrecto, ingrese el codigo: ");
-            scanf("%d", &auxCodigo);	
-         }
-        productos[contProductos].id = auxCodigo;
-        return auxCodigo;
+    while(auxCodigo<0)
+        {
+        printf("Codigo incorrecto, ingrese el codigo: ");
+        scanf("%d", &auxCodigo);	
+        }
+    //fin validación
+
+    productos[contProductos].id = auxCodigo;
+    return auxCodigo;
 }
 
 void nombre()
 {
     printf("Nombre: ");
-        scanf(" %s", productos[contProductos].name); //<- guarda el valor ingresado en la variable name dentro de la estructura Producto
+    scanf(" %s", productos[contProductos].name); //<- guarda lo ingresado en la variable name dentro de la estructura Producto
 }
 
 int tamano()
 {
     printf("Tamano: g");
-        scanf("%f", &auxTamano); //<- guarda el valor ingresado en la variable tamaño dentro de la estructura Producto
+    scanf("%i", &auxTamano);
+
     //validación del tamaño
-        while(auxTamano<0)
-         {
-            printf("Tamano incorrecto, ingrese el tamano: ");
-            scanf("%f", &auxTamano);	
-         }
-        productos[contProductos].tamano = auxTamano;
-        return auxTamano;
+    while(auxTamano<0)
+        {
+        printf("Tamano incorrecto, ingrese el tamano: ");
+        scanf("%i", &auxTamano);	
+        }
+    //fin validación
+
+    productos[contProductos].tamano = auxTamano;
+    return auxTamano;
 }
 
 int stock()
 {
     printf("Stock: ");
-    scanf("%d", &auxCantidad); //<- guarda el valor ingresado en la variable stock dentro de la estructura Producto
+    scanf("%d", &auxCantidad);
+
     //validación del stock
-    while(auxCantidad<0)
-        {
-        printf("Stock incorrecto, ingrese el stock: ");
-        scanf("%d", &auxCantidad);	
-        }
+    while(auxCantidad<1)
+    {
+    printf("Stock incorrecto, ingrese el stock: ");
+    scanf("%d", &auxCantidad);	
+    }
+    //fin validación
+
     productos[contProductos].stock = auxCantidad;
     return auxCantidad;
 }
@@ -128,27 +146,45 @@ int stock()
 float precio()
 {
     printf("Precio: $");
-    scanf("%f", &auxPrecio); //<- guarda el valor ingresado en la variable precio dentro de la estructura Producto
+    scanf("%f", &auxPrecio);
+
     //validación del precio
-    while(auxPrecio<0)
-        {
-        printf("Precio incorrecto, ingrese el precio: ");
-        scanf("%f", &auxPrecio);	
-        }
-    productos[contProductos].precio = auxPrecio;
+    while(auxPrecio<1)
+    {
+    printf("Precio invalido, ingrese el precio: ");
+    scanf("%f", &auxPrecio);	
+    }
+    //fin validación
+    
+    productos[contProductos].precio=auxPrecio;
     return auxPrecio;
 }
 
 void fechaIngreso()
 {
     printf("Fecha de Ingreso (DD/MM/AA): ");
-    scanf(" %s", productos[contProductos].fechaIngrego); //<- guarda el valor ingresado en la variable fecha de Ingreso dentro de la estructura Producto
+    scanf("%d/%d/%d", &productos[contProductos].dia, &productos[contProductos].mes, &productos[contProductos].anio);
+    while (productos[contProductos].dia < 1 || productos[contProductos].dia>31)
+    {
+        printf("Dia invalido, ingrese el dia: ");
+        scanf("%d", &productos[contProductos].dia);
+    }
+    while (productos[contProductos].mes < 1 || productos[contProductos].mes>12)
+    {
+        printf("Mes invalido, ingrese el mes: ");
+        scanf("%d", &productos[contProductos].mes);
+    }
+    while (productos[contProductos].anio < 1 || productos[contProductos].anio>99)
+    {
+        printf("Anio invalido, ingrese el anio: ");
+        scanf("%d", &productos[contProductos].anio);
+    }
 }
 
 void nombreProveedor()
 {
     int p;
-    printf("Nombre del proveedor: ");
+    printf("Nombre del proveedor: \n");
     
     for (p = 0; p < MAX_PROVEEDORES; p++)
     {
@@ -158,10 +194,33 @@ void nombreProveedor()
 
     scanf("%d", &auxProveedor); //<- guarda el valor ingresado en la variable nombreProveedor dentro de la estructura Producto
     
-    while(auxProveedor<0 || auxProveedor>=MAX_PROVEEDORES)
+    while(auxProveedor<1 || auxProveedor>MAX_PROVEEDORES)
     {
         printf("Opcion invalida, ingrese el nombre del proveedor: ");
         scanf("%d", &auxProveedor);	
+    }
+
+    auxProveedor--;
+
+    printf("Seleccionaste: %s\n", proveedores[auxProveedor].nombre);
+    printf("RIF: %d\n", proveedores[auxProveedor].rif);
+    printf("Nombre: %s\n", proveedores[auxProveedor].nombre);
+    printf("Codigo: %s\n", proveedores[auxProveedor].codigo);
+
+    productos[contProductos].nombreProveedor[15] = proveedores[auxProveedor].nombre[15]; //<- guarda el nombre del proveedor seleccionado en el campo nombreProveedor de la estructura Producto
+}
+
+int activo()
+{
+    if (productos[contProductos].stock > 0)
+    {
+        productos[contProductos].activo = 1;
+        return 1;
+    }
+    else
+    {
+        productos[contProductos].activo = 0;
+        return 0;
     }
 }
 
@@ -177,9 +236,7 @@ void registrarProducto()//función para registrar un nuevo producto en el invent
         precio();
         fechaIngreso();
         nombreProveedor();
-
-        printf("Disponible (1 si) (0 no): ");
-        scanf("%d", &productos[contProductos].activo); //<- guarda el valor ingresado en la variable activo dentro de la estructura Producto
+        activo();
         contProductos++; //<- incrementa el contador de productos para que el siguiente producto se guarde en la siguiente posición del arreglo productos
     }
     else
@@ -193,20 +250,20 @@ void mostrarProducto()//función para mostrar info general de todos los producto
     int i; //variable de control para el ciclo for
 
     noHallado(); //<- llamada a la función noHallado para verificar si el inventario está vacío antes de intentar mostrar los productos
-
-    printf("==================== INVENTARIO ACTUAL ====================\n");
-    printf("%-5s %-15s %-10s %-10s %-8s\n", "ID", "NOMBRE", "PRECIO", "STOCK", "ESTADO"); //<- encabezado de la tabla que se muestra al usuario, con formato para alinear ID, Nombre, Precio, Stock y Estado
-    printf("-----------------------------------------------------------\n");
-    for (i = 0; i < MAX_PRODUCTOS; i++) //ciclo for para recorrer el arreglo de productos y mostrar la información de cada producto registrado en el inventario
+    if (contProductos != 0)
     {
-        printf("%-5d %-15s %-10.2f %-10d %-8s\n", 
-                productos[i].id, 
-                productos[i].name, productos[i].precio, 
-                productos[i].stock, 
-                (productos[i].activo == 1) ? "Disponible" : "no disponible" //<- muestra "Disponible" si el valor de activo es 1 y "no disponible" si el valor de activo es 0, utilizando un operador ternario para determinar qué texto mostrar en función del estado del producto
-              );
+        printf("==================== INVENTARIO ACTUAL ====================\n");
+        printf("%-5s %-15s %-10s %-10s %-8s\n", "ID", "NOMBRE", "PRECIO", "STOCK", "ESTADO"); //<- encabezado de la tabla que se muestra al usuario, con formato para alinear ID, Nombre, Precio, Stock y Estado
+        printf("-----------------------------------------------------------\n");
+        for (i = 0; i < MAX_PRODUCTOS; i++) //ciclo for para recorrer el arreglo de productos y mostrar la información de cada producto registrado en el inventario
+        {
+            if (productos[i].activo == 1)
+            {
+                printf("%-5d %-15s $%-10.2f %-10d %-8s\n", productos[i].id, productos[i].name, productos[i].precio, productos[i].stock, (productos[i].activo == 1) ? "Disponible" : "no disponible"); //<- muestra "Disponible" si el valor de activo es 1 y "no disponible" si el valor de activo es 0, utilizando un operador ternario para determinar qué texto mostrar en función del estado del producto
+            }
+        }
+        printf("===========================================================\n");
     }
-    printf("===========================================================\n");
 }
 
 int consultar() //función para consultar los detalles de un producto específico mostrando toda la info
@@ -219,21 +276,24 @@ int consultar() //función para consultar los detalles de un producto específic
 
     noHallado();
      
-    for (k = 0; k < contProductos; k++) //ciclo for para recorrer el arreglo de productos y buscar el producto con el ID ingresado por el usuario
+    if (contProductos != 0)
     {
-        if (productos[k].id == buscarID)// condición para verificar si el ID del producto en la posición k del arreglo productos coincide con el ID ingresado por el usuario, si se encuentra una coincidencia, se muestra la información detallada del producto
+        for (k = 0; k < contProductos; k++) //ciclo for para recorrer el arreglo de productos y buscar el producto con el ID ingresado por el usuario
         {
-            printf("\n--- DETALLES DEL PRODUCTO ---\n");
-            printf("ID:        %d\n", productos[k].id);
-            printf("Nombre:    %s\n", productos[k].name);
-            printf("Tamano:    %.2f g\n", productos[k].tamano);
-            printf("Stock:     %d unidades\n", productos[k].stock);
-            printf("Precio:    $%.2f\n", productos[k].precio);
-            printf("Fecha Ing: %s\n", productos[k].fechaIngrego);
-            printf("Proveedor: %s\n", productos[k].nombreProveedor);
-            printf("Estado:    %s\n", (productos[k].activo == 1) ? "Activo" : "Inactivo");
-            printf("-----------------------------\n");
-            break;
+            if (productos[k].id == buscarID)// condición para verificar si el ID del producto en la posición k del arreglo productos coincide con el ID ingresado por el usuario, si se encuentra una coincidencia, se muestra la información detallada del producto
+            {
+                printf("\n------------------ DETALLES DEL PRODUCTO ------------------\n");
+                printf("\t\t\tID:        %d\n", productos[k].id);
+                printf("\t\t\tNombre:    %s\n", productos[k].name);
+                printf("\t\t\tTamano:    %.2f gramos\n", productos[k].tamano);
+                printf("\t\t\tStock:     %d unidades\n", productos[k].stock);
+                printf("\t\t\tPrecio:    $%.2f\n", productos[k].precio);
+                printf("\t\t\tFecha de ingreso: %02d/%02d/%04d\n", productos[k].dia, productos[k].mes, productos[k].anio);
+                printf("\t\t\tProveedor: %s\n", productos[k].nombreProveedor);
+                printf("\t\t\tEstado:    %s\n", (productos[k].activo == 1) ? "Activo" : "Inactivo");
+                printf("--------------------------------------------------------------\n");
+                break;
+            }
         }
     }
     return buscarID;
@@ -243,11 +303,10 @@ void noHallado()
 {
     if (contProductos == 0)
     {
-        printf("\nEl inventario esta vacio.\n");
-        return;
+        printf("===========================================================\n");
+        printf("\nEl inventario esta vacio.\n\n");
     }
 }
-
 //CODIGO PRINCIPAL
 int main()
 {
